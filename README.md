cd8824e6923ef3f4c146f7c3163e3fe434499654
arielbk <buchwald.kearney@gmail.com>
2020-08-06 15:35:00 +0200

    Add small readme

---
+++ README.md
@@ -0,0 +1,8 @@
+Following along with the [super tiny compiler](https://github.com/jamiebuilds/the-super-tiny-compiler)
+Definitely check that out!
+
+This is a really good exercise to understand how code is parsed and executed,
+it brings to mind what I've read on JavaScript interpretation
+
+Unlike the original, this has been split by functionality into CommonJS modules.
+Check out the starting point, [index.js](index.js).
--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 /**
- * Following along with the super tiny compiler (https://github.com/jamiebuilds/the-super-tiny-compiler)
- * Definitely check it out!
- * 
- * This seems like a really good exercise even just to understand how code is parsed and executed,
- * it brings to mind what I've read on JavaScript interpretation
- * 
  * Basic compilation of Lisp-like function calls to C-like function calls:
  * (add 3 4) => add(3, 4)
  * (add 3 (subtract 9 7)) => add(3, subtract(9, 7))
